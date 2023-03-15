ba9cf789e8ef621c1d696a59e6008dd5d061eb39
Fadhil-AS <mfadhil661@gmail.com>
2023-03-15 16:10:07 +0700

    menambahkan class KodeBuah

---
+++ modul4_1302210006/KodeBuah_1302210006.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302210006
+{
+    internal class KodeBuah_1302210006
+    {
+        public enum buah {
+            Apel,
+            Aprikot,
+            Alpukat,
+            Pisang,
+            Paprika,
+            Blackberry,
+            Ceri,
+            Kelapa,
+            Jagung
+        }
+
+        public static String getKodeBuah_1302210006(buah fruit) {
+            string[] kode_Buah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00" };
+            return kode_Buah[(int)fruit];
+        }
+
+    }
+}
--- modul4_1302210006/Program.cs
+++ modul4_1302210006/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using modul4_1302210006;
+
+KodeBuah_1302210006.buah Buah = KodeBuah_1302210006.buah.Apel;
+string kBuah = KodeBuah_1302210006.getKodeBuah_1302210006(Buah);
+Console.WriteLine("Nama buah: " + Buah + " \nKode Buah: " + kBuah);
